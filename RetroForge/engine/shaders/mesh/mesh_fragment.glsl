15fc0146a09119b09e1a1b12cedb9396cac79fc7
mika basten <mikabasten@gmail.com>
2024-07-07 03:59:59 +0200

    Revert "fixed shaders so now we can draw objects"
    This reverts commit 291c93e0d26cd250cd47634de89c634d2cfc82eb.

--- RetroForge/engine/shaders/mesh/mesh_fragment.glsl
+++ RetroForge/engine/shaders/mesh/mesh_fragment.glsl
@@ -1,7 +1,24 @@
-#version 330 core
+#version 450 core
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
 out vec4 FragColor;
 
-void main()
-{
-    FragColor = vec4(1.0, 0.0, 0.0, 1.0); // Red color
+uniform sampler2D texture_diffuse;
+
+void main() {
+    vec3 lightColor = vec3(1.0);
+    vec3 lightPos = vec3(0.0, 0.0, 10.0);
+    vec3 viewPos = vec3(0.0, 0.0, 5.0);
+
+    vec3 ambient = 0.1 * lightColor;
+    
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    vec3 result = (ambient + diffuse) * texture(texture_diffuse, TexCoords).rgb;
+    FragColor = vec4(result, 1.0);
 }
--- RetroForge/game/src/Main.cpp
+++ RetroForge/game/src/Main.cpp
@@ -27,49 +27,11 @@ int main(int argc, char* argv[])
         0, 1, 2
     };
 
-    std::vector<Vertex> icosahedronVertices = {
-    {{-0.525731112119133606f, 0.0f, 0.850650808352039932f}, {1.0f, 0.0f, 0.0f}}, // Red
-    {{0.525731112119133606f, 0.0f, 0.850650808352039932f}, {0.0f, 1.0f, 0.0f}}, // Green
-    {{-0.525731112119133606f, 0.0f, -0.850650808352039932f}, {0.0f, 0.0f, 1.0f}}, // Blue
-    {{0.525731112119133606f, 0.0f, -0.850650808352039932f}, {1.0f, 1.0f, 1.0f}}, // White
-    {{0.0f, 0.850651f, 0.525731f}, {1.0f, 0.0f, 0.0f}}, // Red
-    {{0.0f, 0.850651f, -0.525731f}, {0.0f, 1.0f, 0.0f}}, // Green
-    {{0.0f, -0.850651f, 0.525731f}, {0.0f, 0.0f, 1.0f}}, // Blue
-    {{0.0f, -0.850651f, -0.525731f}, {1.0f, 1.0f, 1.0f}}, // White
-    {{0.850651f, 0.525731f, 0.0f}, {1.0f, 0.0f, 0.0f}}, // Red
-    {{-0.850651f, 0.525731f, 0.0f}, {0.0f, 1.0f, 0.0f}}, // Green
-    {{0.850651f, -0.525731f, 0.0f}, {0.0f, 0.0f, 1.0f}}, // Blue
-    {{-0.850651f, -0.525731f, 0.0f}, {1.0f, 1.0f, 1.0f}} // White
-    };
-
-    std::vector<unsigned int> icosahedronIndices = {
-        0, 4, 1,
-        0, 9, 4,
-        9, 5, 4,
-        4, 5, 8,
-        4, 8, 1,
-        8, 10, 1,
-        8, 3, 10,
-        5, 3, 8,
-        5, 2, 3,
-        2, 7, 3,
-        7, 10, 3,
-        7, 6, 10,
-        7, 11, 6,
-        11, 0, 6,
-        0, 1, 6,
-        6, 1, 10,
-        9, 0, 11,
-        9, 11, 2,
-        9, 2, 5,
-        7, 2, 11
-    };
-
     std::shared_ptr<Mesh> triangleMesh = std::make_shared<Mesh>(vertices, indices);
-    std::shared_ptr<Mesh> icosahedronMesh = std::make_shared<Mesh>(icosahedronVertices, icosahedronIndices);
+
     engine.Initialize();
 
-    engine.RenderObject(icosahedronMesh);
+    engine.RenderObject(triangleMesh);
 
     engine.Run();
     return 0;
